bb5d32b3d116f6bb536e4eb4c608a6b89d251b91
HP <dhiachabane@yahoo.fr>
2023-07-16 02:34:37 +0200

    fixing require bug using maj letters

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require("mongoose");
-const compression = require("compression");
+const mongoose = require('mongoose');
+const compression = require('compression');
 const cookieParser = require('cookie-parser');
 
 // const multer = require("multer");
 // const uploadMiddleware = require("./middlewares/upload");
 const cors = require('cors');
-const { loadRoutes } = require('./utils/loadRoutes');
+const { loadRoutes } = require('./utils/loadRoutes.js');
 require('dotenv').config();
 const path = require('path');
 
 const app = express();
-const http = require("http").Server(app);
+const http = require('http').Server(app);
 const port = process.env.PORT || 8000;
 const CONNECTION_URL = process.env.CONNECTION_URL;
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  })
+);
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
@@ -37,7 +39,8 @@ app.get('/*', (req, res) => {
 loadRoutes(app, './routes');
 
 // Connect to MongoDB and start the server
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     http.listen(port, () => {
       console.log(`Server is running on port http://localhost:${port}`);
